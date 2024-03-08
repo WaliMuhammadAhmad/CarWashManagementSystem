71a9ef774a739439e15f9882807dce47d9393290
WaliMuhammadAhmad <2021se39student.uet.edu.pk>
2024-03-08 19:20:48 +0500

    Home page created + images added

--- code/Form1.cs
+++ code/Form1.cs
@@ -16,5 +16,73 @@ namespace code
         {
             InitializeComponent();
         }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnDashboard.Height;
+            panelSlide.Top = btnDashboard.Top;
+        }
+
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnEmployees.Height;
+            panelSlide.Top = btnEmployees.Top;
+        }
+
+        private void btnCustomers_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnCustomers.Height;
+            panelSlide.Top = btnCustomers.Top;
+        }
+
+        private void btnServices_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnServices.Height;
+            panelSlide.Top = btnServices.Top;
+        }
+
+        private void btnCash_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnCash.Height;
+            panelSlide.Top = btnCash.Top;
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnReport.Height;
+            panelSlide.Top = btnReport.Top;
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnSetting.Height;
+            panelSlide.Top = btnSetting.Top;
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnLogout.Height;
+            panelSlide.Top = btnLogout.Top;
+        }
     }
 }
